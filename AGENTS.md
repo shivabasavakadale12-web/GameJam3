01e065cd60023e6a763f3372e0d89919159c22a1
Shivabasava <shivabasavakadale12@gmail.com>
2026-09-03 23:42:06 +0530

    Player movement and sprint through input action!

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: GameJam3
+- Unity version: Unity 6000.3.10f1
+- Active game object:
+  - Name: Idle_0
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float movespeed = 5f;
+    float sprintmultiplier = 1.5f;
+    Vector2 movement;
+    Rigidbody2D rb;
+    bool isSprint = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnPlayermovement(CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        Debug.Log("PlayerMovement: " + movement);
+    }
+
+    public void OnSprint(CallbackContext context)
+    {
+        isSprint = context.ReadValueAsButton();
+        if (isSprint)
+        {
+            movespeed *= sprintmultiplier;
+        }
+        else
+        {
+            movespeed = 5f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 currentposition = rb.position;
+        Vector2 direction = new Vector2(movement.x * movespeed , 0);
+
+        rb.MovePosition(currentposition + direction * Time.fixedDeltaTime);
+    }
+}
