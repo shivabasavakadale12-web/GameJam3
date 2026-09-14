9bfbd3ec5a1f0698939ce5103215197b5d14bcb0
Shivabasava <shivabasavakadale12@gmail.com>
2026-09-14 15:31:23 +0530

    Advanced enemy ai idle, walk and run animation trigger through bool

--- Assets/scripts/AdvancedEnemyAI.cs
+++ Assets/scripts/AdvancedEnemyAI.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class AdvancedEnemyAI : MonoBehaviour
 {
-    [SerializeField] float MoveSpeed;
     [SerializeField] EnemyData enemyData;
     Transform playerTransform;
     Rigidbody2D rb;
     Animator animator;
     float distance;
+    float runspeed = 1.7f;
 
+    BoxCollider2D[] hitbox;
+
+    bool isAttacking = false;
+    bool isDefending = false;
+    const string walk = "Walk";
+    const string run = "Run";
     void Start()
     {
+        hitbox = GetComponentsInChildren<BoxCollider2D>();
+        hitbox[0].enabled = false;
+        hitbox[1].enabled = false;
+        hitbox[2].enabled = false;
+        hitbox[3].enabled = false;
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -25,13 +36,28 @@ public class AdvancedEnemyAI : MonoBehaviour
 
         if (distance <= enemyData.attackRange)
         {
+            animator.SetBool(walk, false);
+            animator.SetBool(run, false);
             rb.linearVelocity = Vector2.zero;
-            Debug.Log(distance.ToString() + " / " + enemyData.attackRange);
+            isAttacking = true;
         }
-        else
+        else if (distance > enemyData.rundistance)
         {
+            animator.SetBool(run, true);
+            animator.SetBool(walk, false);
+            rb.linearVelocity = direction * enemyData.moveSpeed * runspeed;
+        }
+        else if (distance > enemyData.walkdistance)
+        {
+            animator.SetBool(walk, true);
+            animator.SetBool(run, false);
             rb.linearVelocity = direction * enemyData.moveSpeed;
-            Debug.Log(distance.ToString() + " / " + enemyData.attackRange);
+        }
+
+        if (isAttacking)
+        {
+            animator.SetBool(walk, false);
+            animator.SetBool(run, false);
         }
 
     }
